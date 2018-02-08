c29f6cc620c2c80d929990f5fb35f4abd831a853
Eduardo Gomez <eduardo.www@gmail.com>
2018-02-07 20:46:58 -0600

    Added Console Log to DEMO function.

--- src/functions/demo.ts
+++ src/functions/demo.ts
@@ -1,4 +1,10 @@
 'use strict';
 import { success, failure, notAllowed } from './../libs/response-lib';
 
-export async function demo(event, context, callback) {}
+export async function demo(event, context, callback) {
+  console.log(
+    'Tocayolog:' + JSON.stringify(event),
+    JSON.stringify(context),
+    JSON.stringify(callback)
+  );
+}
