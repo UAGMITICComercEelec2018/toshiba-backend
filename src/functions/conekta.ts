ae5f809fb0fc424663e20d1f90e64642bab154d5
Eduardo Gomez <eduardo.www@gmail.com>
2018-03-07 20:46:30 -0600

    Copy local file Serverless.env.yml

--- src/functions/conekta.ts
+++ src/functions/conekta.ts
@@ -6,7 +6,38 @@ conekta.api_version = '2.0.0';
 
 export async function createOxxoCharge(event, context, callback) {
   console.log(conekta);
-  return callback{null,success{}};
+  order = conekta.Order.create({
+    "line_items": [{
+        "name": "Tacos",
+        "unit_price": 1000,
+        "quantity": 12
+    }],
+    "shipping_lines": [{
+        "amount": 1500,
+        "carrier": "FEDEX"
+    }], //shipping_lines - phyiscal goods only
+    "currency": "MXN",
+    "customer_info": {
+      "name": "Fulanito Pérez",
+      "email": "<a href="mailto:fulanito@conekta.com">fulanito@conekta.com</a>",
+      "phone": "+5218181818181"
+    },
+    "shipping_contact":{
+       "address": {
+         "street1": "Calle 123, int 2",
+         "postal_code": "06100",
+         "country": "MX"
+       }
+    }, //shipping_contact - required only for physical goods
+    "charges":[{
+      "payment_method": {
+        "type": "oxxo_cash"
+      }
+    }]
+  }, function(err, res) {
+      console.log(res.toObject());
+  });
+  return order; //callback{null,success{}};
 }
 
 export async function conektaWebhook(event, context, callback) {
